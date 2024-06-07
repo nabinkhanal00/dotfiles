e3036c2a19d14859ef703140651abd363ebefeb8
Nabin Khanal <nabinkhanal@skiff.com>
2024-06-07 21:46:13 +0545

    added zsh

---
+++ zsh/.zsh/init.sh
@@ -0,0 +1,7 @@
+source "$(dirname "$0")/plugins/init.sh"
+source "$(dirname "$0")/fzf.sh"
+source "$(dirname "$0")/env.sh"
+source "$(dirname "$0")/aliases.sh"
+source "$(dirname "$0")/functions.sh"
+source "$(dirname "$0")/variables.sh"
+source "$(dirname "$0")/paths.sh"
---
+++ zsh/.zsh/plugins/init.sh
@@ -0,0 +1,3 @@
+source "$(dirname "$0")/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$(dirname "$0")/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+source "$(dirname "$0")/fzf-git.sh/fzf-git.sh"
