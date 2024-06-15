c103500db058977115fa370e801192b7bf9fef97
Nabin Khanal <nabinkhanal@skiff.com>
2024-06-15 20:12:05 +0545

    added few more nvim plugins

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		init = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						require('lspconfig')[server_name].setup({})
+					end,
+				}
+			})
+		end
+	}
+}
